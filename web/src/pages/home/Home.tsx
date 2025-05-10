a9d67fc16c1cebc60058525966b5501788e14607
JJWeixel <weixeljohn@gmail.com>
2025-05-10 14:49:17 -0400

    homeheader

--- web/src/pages/home/Home.tsx
+++ web/src/pages/home/Home.tsx
@@ -1,22 +1,20 @@
-import { Button, buttonVariants } from "@/components/ui/button"
+import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label"  
 import { Switch } from "@/components/ui/switch"
+import HomeHeader from "./components/HomeHeader"
 
   
 const Home = () => {
     return (
-        <div className="w-screen h-screen flex flex-col gap-4 justify-items-center bg-sky-200">
-            <div className="flex flex-row pt-4 justify-start pl-10 items-baseline text-7xl">
-                <div className="text-shadow-md">LakeTracker</div>
-                <div className="text-base text-gray-600 pl-4">Lake Erie</div>
-            </div>
+        <div className="w-screen h-screen flex flex-col gap-4 justify-items-center">
+            <HomeHeader/>
             <div className="flex flex-row grow">
                 <div className="w-60 flex flex-col justify-self-start justify-start justify-items-start text-gray-600 text-2xl bg-white inset-shadow-xl/30">
                     <div className="w-full h-10 flex text-sm justify-center font-bold text-black items-center bg-gray-200">Menu</div>
                     <Button variant="ghost" className="w-full text-sm text-gray-600 font-sans justify-start">Settings</Button>
-                    <div className="flex items-center justify-start pl-4">
-                        <Label>Toggle dark mode</Label>
-                        <Switch></Switch>
+                    <div className="flex items-center justify-between pl-4">
+                        <Label htmlFor="theme-toggle">Toggle dark mode</Label>
+                        <Switch id="theme-toggle"></Switch>
                     </div>
                     <Button variant="ghost" className="w-full text-sm text-gray-600 font-sans justify-start">About</Button>
 
---
+++ web/src/pages/home/components/HomeHeader.tsx
@@ -0,0 +1,10 @@
+
+const HomeHeader = () => {
+    return (
+        <div className="sticky flex top-8 left-0 right-0 h-24 w-5/6">
+            <span className="text-5xl font-semibold">LakeTracker</span>
+        </div>
+    )
+}
+
+export default HomeHeader
