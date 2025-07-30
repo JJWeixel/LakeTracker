d933420219f122a2f43c04d4236a8f0920d0bcfc
JoeyWeixel <joeyweixel@gmail.com>
2025-07-30 19:59:13 -0400

    basic weather controller

---
+++ api/Endpoints/Weather/WeatherController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Exception = api.Exceptions.Exception;
+
+namespace api.Endpoints.Weather;
+
+[ApiController]
+[Route("/weather")]
+public class WeatherController : BaseApiController
+{
+    private readonly WeatherServices _services;
+    public WeatherController(WeatherServices services)
+    {
+        _services = services;
+    }
+    
+    [HttpGet()]
+    public IActionResult GetWeather()
+    {
+        try
+        {
+            var weather = _services.GetWeather();
+            return Ok(weather);
+        }
+        catch (Exception ex)
+        {
+            HandleException(ex);
+        }
+    }
+    
+}
