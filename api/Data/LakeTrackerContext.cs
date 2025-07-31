80f0673440a082a0fbf8049ed1266746346f4371
JoeyWeixel <joeyweixel@gmail.com>
2025-07-30 20:28:08 -0400

    local api with one endpoint

--- api/Data/LakeTrackerContext.cs
+++ api/Data/LakeTrackerContext.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace api.Data
 {
-    public class LakeTrackerContext
+    public class LakeTrackerContext : DbContext
     {
-        
+        public LakeTrackerContext(DbContextOptions<LakeTrackerContext> options)
+            : base(options)
+        {
+            
+        }
     }
 }
--- api/Program.cs
+++ api/Program.cs
@@ -1,15 +1,101 @@
+using System.Security.Claims;
+using api.Data;
+using api.Endpoints.Weather;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authorization;
+
 namespace api
 {
-    public class Program
+        public class Program
     {
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
 
-            app.Run();
+            AddApiExplorer(builder);
+            AddSwaggerGen(builder);
+            AddDbContext(builder);
+            AddServices(builder);
+            AddControllers(builder);
+            AddCurrentUser(builder);
+            AddCorsPolicy(builder);
+
+            RunApp(builder);
         }
         
         
+
+        private static void AddApiExplorer(WebApplicationBuilder builder)
+        {
+            builder.Services.AddEndpointsApiExplorer();
+        }
+
+        private static void AddSwaggerGen(WebApplicationBuilder builder)
+        {
+            builder.Services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc("v1", new OpenApiInfo { Title = "LakeTrackerApi", Version = "v1" });
+            });
+        }
+
+        private static void AddDbContext(WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<LakeTrackerContext>();
+        }
+
+        private static void AddServices(WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<WeatherServices>();
+        }
+
+        private static void AddControllers(WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+        }
+        
+        private static void AddCurrentUser(WebApplicationBuilder builder)
+        {
+            builder.Services.AddTransient<IHttpContextAccessor, HttpContextAccessor>();
+            builder.Services.AddTransient(sp =>
+            {
+                var accessor = sp.GetRequiredService<IHttpContextAccessor>();
+                var user = accessor?.HttpContext?.User;
+                return user ?? throw new InvalidOperationException("User not found");
+            });
+        }
+
+        private static void AddCorsPolicy(WebApplicationBuilder builder)
+        {
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                    policy
+                        .WithOrigins("http://localhost:5173") //TODO: Update with your frontend URL
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+            });
+        }
+
+        private static void RunApp(WebApplicationBuilder builder)
+        {
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+                app.UseCors("AllowAll");
+            }
+            else 
+            {
+                app.UseCors("AllowAll");
+            }
+
+            app.UseHttpsRedirection();
+            app.MapControllers();
+            
+            app.Run();
+        }
     }
 }
