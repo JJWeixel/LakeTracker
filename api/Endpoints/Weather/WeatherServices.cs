6335fd27cd342975aa2a2a0de207712479e1342e
JoeyWeixel <joeyweixel@gmail.com>
2025-08-02 16:21:20 -0400

    basic fetching

--- api/Endpoints/Weather/WeatherServices.cs
+++ api/Endpoints/Weather/WeatherServices.cs
@@ -1,5 +1,7 @@
 using System.Security.Claims;
 using api.Data;
+using api.Endpoints.Weather.RequestResponse.NoaaTemperature;
+using api.Endpoints.Weather.RequestResponse.NoaaWater;
 
 namespace api.Endpoints.Weather;
 
@@ -19,10 +21,44 @@ public class WeatherServices : BaseService
         var NOAAWeatherClient = new HttpClient();
         NOAAWeatherClient.BaseAddress = new Uri("https://api.tidesandcurrents.noaa.gov/api/prod/datagetter");
 
-        NOAAWeatherClient.GetAsync(
+        var waterRequest = await NOAAWeatherClient.GetAsync(
             "?date=today&station=9063063&product=water_temperature&time_zone=LST_LTD&interval=h&units=english&application=DataAPI_Sample&format=json");
-        // Simulate fetching weather data
-        // In a real application, this would involve database access or an external API call
-        return "Sunny, 25°C";
+        
+        if (!waterRequest.IsSuccessStatusCode)
+        {
+            Logger.LogError("Failed to fetch weather data from NOAA API.");
+            return "Error fetching weather data.";
+        }
+        
+        var windRequest = await NOAAWeatherClient.GetAsync(
+            "?date=today&station=9063063&product=wind&time_zone=LST_LTD&interval=h&units=english&application=DataAPI_Sample&format=json");
+        
+        if (!windRequest.IsSuccessStatusCode)
+        {
+            Logger.LogError("Failed to fetch wind data from NOAA API.");
+            return "Error fetching wind data.";
+        }
+        
+        var temperatureRequest = await NOAAWeatherClient.GetAsync(
+            "?date=today&station=9063063&product=air_temperature&time_zone=LST_LTD&interval=h&units=english&application=DataAPI_Sample&format=json");
+        
+        if (!temperatureRequest.IsSuccessStatusCode)
+        {
+            Logger.LogError("Failed to fetch air temperature data from NOAA API.");
+            return "Error fetching air temperature data.";
+        }
+        
+        var waterData = await waterRequest.Content.ReadFromJsonAsync<NoaaWeatherResponseWater>();
+        var windData = await windRequest.Content.ReadFromJsonAsync<NoaaWeatherResponseWind>();
+        var temperatureData = await temperatureRequest.Content.ReadFromJsonAsync<NoaaWeatherResponseTemperature>();
+        
+        
+        
+        
+        
     }
+}
+
+public class NoaaWeatherResponseTemperature
+{
 }
