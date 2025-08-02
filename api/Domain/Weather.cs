cf7e421ff28539063f7014d83e63d1f3e42bb4ed
JJWeixel <weixeljohn@gmail.com>
2025-08-02 16:36:44 -0400

    idk

--- api/Domain/Weather.cs
+++ api/Domain/Weather.cs
@@ -2,13 +2,13 @@ namespace api.Domain;
 
 public class Weather
 {
-    public float AirTemperature { get; set; }
-    public float WaterTemperature { get; set; }
-    public float WindSpeed { get; set; }
-    public float WindDirection { get; set; }
+    public double AirTemperature { get; set; }
+    public double WaterTemperature { get; set; }
+    public double WindSpeed { get; set; }
+    public double WindDirection { get; set; }
     public string WindDirectionReadable { get; set; }
-    public float GustSpeed { get; set; }
-    public float WaveHeight { get; set; }
-    public float DominantWavePeriod { get; set; }
-    public float Visibility { get; set; }
+    public double GustSpeed { get; set; }
+    public double WaveHeight { get; set; }
+    public double DominantWavePeriod { get; set; }
+    public double Visibility { get; set; }
 }
