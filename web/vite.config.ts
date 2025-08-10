25a68b59f8e772c6fdc7e1b120f2970cd5eea1a9
JJWeixel <weixeljohn@gmail.com>
2025-08-10 13:26:16 -0400

    fix

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/LakeTracker',
+  base: '/LakeTracker/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
